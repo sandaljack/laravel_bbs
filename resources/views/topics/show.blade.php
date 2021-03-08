3173569aad1b961ed4395611d65a2fb7debee328
lxj <425862717@qq.com>
2021-03-08 16:13:09 +0800

    回复列表

--- resources/views/topics/show.blade.php
+++ resources/views/topics/show.blade.php
@@ -35,21 +35,33 @@
                 <div class="topic-body mt-4 mb-4">
                 {!! $topic->body !!}
                 </div>
-                <div class="operate">
-                    <hr>
-                    <a href="{{ route('topics.edit', $topic->id) }}" class="btn btn-outline-secondary btn-sm" role="button">
-                        <i class="far fa-edit"></i> 编辑
-                    </a>
-                    <form action="{{ route('topics.destroy', $topic->id) }}" method="post"
-                        style="display: inline-block;"
-                        onsubmit="return confirm('您确定要删除吗？');">
-                        {{ csrf_field() }}
-                        {{ method_field('DELETE') }}
-                        <button type="submit" class="btn btn-outline-secondary btn-sm">
-                        <i class="far fa-trash-alt"></i> 删除
-                        </button>
-                    </form>
-                </div>
+
+                @can('update', $topic)
+                    <div class="operate">
+                        <hr>
+                        <a href="{{ route('topics.edit', $topic->id) }}" class="btn btn-outline-secondary btn-sm" role="button">
+                            <i class="far fa-edit"></i> 编辑
+                        </a>
+                        <form action="{{ route('topics.destroy', $topic->id) }}" method="post"
+                            style="display: inline-block;"
+                            onsubmit="return confirm('您确定要删除吗？');">
+                            {{ csrf_field() }}
+                            {{ method_field('DELETE') }}
+                            <button type="submit" class="btn btn-outline-secondary btn-sm">
+                            <i class="far fa-trash-alt"></i> 删除
+                            </button>
+                        </form>
+                    </div>
+                @endcan
+
+            </div>
+        </div>
+
+        {{-- 用户回复列表 --}}
+        <div class="card topic-reply mt-4">
+            <div class="card-body">
+                @include('topics._reply_box', ['topic' => $topic])
+                @include('topics._reply_list', ['replies' => $topic->replies()->with('user')->get()])
             </div>
         </div>
     </div>
