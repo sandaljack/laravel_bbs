6feb30100557efffabd3ea55b4a7faa3af745a7c
lxj <425862717@qq.com>
2021-03-09 09:46:33 +0800

    用户可以发表评论

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -3,19 +3,13 @@
 namespace App\Observers;
 
 use App\Models\Reply;
-
 // creating, created, updating, updated, saving,
-// saved,  deleting, deleted, restoring, restored
-
+// saved, deleting, deleted, restoring, restored
 class ReplyObserver
 {
-    public function creating(Reply $reply)
+    public function created(Reply $reply)
     {
-        //
-    }
-
-    public function updating(Reply $reply)
-    {
-        //
+        $reply->topic->reply_count = $reply->topic->replies->count();
+        $reply->topic->save();
     }
 }
