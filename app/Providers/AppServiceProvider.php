d0c2a72ad49eef33dc765c1f42825303118fc8f4
lxj <425862717@qq.com>
2021-03-10 15:04:27 +0800

    侧边栏链接

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -29,6 +29,8 @@ class AppServiceProvider extends ServiceProvider
 		\App\Models\User::observe(\App\Observers\UserObserver::class);
 		\App\Models\Reply::observe(\App\Observers\ReplyObserver::class);
 		\App\Models\Topic::observe(\App\Observers\TopicObserver::class);
+        \App\Models\Link::observe(\App\Observers\LinkObserver::class);
+
 
         //日期时间扩展中文
         \Carbon\Carbon::setLocale('zh');
--- config/administrator.php
+++ config/administrator.php
@@ -45,6 +45,7 @@ return array(
         ],
         '站点管理' => [
             'settings.site',
+            'links',
         ],
     ],
     /*
